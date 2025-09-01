eb2a69b45bcc91097674425473997eb61fb6a692
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-09-01 16:13:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//your JS code here. If required.
-var button1 = document.getElementById("openModal");
-var modal1= document.getElementsByClassName("modal")[0];
-var bt1=    document.getElementsByClassName("close-modal")[0];
-button1.onclick=function() {
-	modal1.style.display="block";
+// //your JS code here. If required.
+// var button1 = document.getElementById("openModal");
+// var modal1= document.getElementsByClassName("modal")[0];
+// var bt1=    document.getElementsByClassName("close-modal")[0];
+// button1.onclick=function() {
+// 	modal1.style.display="block";
 	
-}
-bt1.onclick=function() {
-modal1.style.display="none";
-}
+// }
+// bt1.onclick=function() {
+// modal1.style.display="none";
+// }
